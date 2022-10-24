6b4021c7c06f2a41f45ddc6a2777f0d71819b709
RakibAlom <rakibalom17@gmail.com>
2022-10-25 03:12:22 +0600

    install cors

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
-const port = process.env.PORT || 5000;
+app.use(cors())
+
+const port = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
-  res.send('Next Learner Server Running...');
+  res.send('Next Learner Server Running...')
 })
 
 app.listen(port, () => {
-  console.log(`Next Learner Server Running on PORT: ${port}`);
+  console.log(`Next Learner Server Running on PORT: ${port}`)
 })
