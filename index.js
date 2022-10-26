3ef37dcb87466a6e1fa3f7a8d1482cd13f21a7ef
RakibAlom <rakibalom17@gmail.com>
2022-10-27 05:01:22 +0600

    blog json data added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.use(cors())
 const port = process.env.PORT || 5000
 
 const courses = require('./data/courses.json')
+const blogs = require('./data/blogs.json')
 
 app.get('/', (req, res) => {
   res.send('Next Learner Server Running...')
@@ -13,6 +14,16 @@ app.get('/', (req, res) => {
 
 app.get('/courses', (req, res) => {
   res.send(courses);
+})
+
+app.get('/courses/:slug', (req, res) => {
+  const slug = req.params.slug;
+  const course = courses.find(item => item.slug === slug)
+  res.send(course)
+})
+
+app.get('/blogs', (req, res) => {
+  res.send(blogs)
 })
 
 app.listen(port, () => {
